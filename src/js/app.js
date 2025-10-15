31af184fc286ed8659dd19cbae8eee65f6450d03
Adrian <adrian.hernandezmorales10@gmail.com>
2025-10-15 11:57:31 -0600

    Implementación de modo obscuro
    Se agrega botón en las páginas para habilitar el modo obscuro utilizando SCSS y JavaScript

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,29 @@
 document,addEventListener('DOMContentLoaded', function() {
     eventListeners();
+    darkMode();
 });
+
+function darkMode(){
+
+    const prefiereDarkMode = window.matchMedia('prefers-color-scheme: dark');
+    
+    if(prefiereDarkMode.matches){
+        document.body.classList.add('dark-mode');
+    }  else {
+        document.body.classList.remove('dark-mode');
+    }
+
+    prefiereDarkMode = addEventListener('change', function() {
+        document.body.classList.toggle('dark-mode');
+    });
+
+    
+    const botonDarkmode = document.querySelector('.dark-mode-boton');
+
+    botonDarkmode.addEventListener('click', function(){
+        document.body.classList.toggle('dark-mode');
+    });
+}
 
 function eventListeners() {
     const mobileMenu = document.querySelector('.mobile-menu');
